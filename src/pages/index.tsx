7a650f43686faa94c3e0e50cc6a2eaf0958c34c4
Alaa Hijazi <alaa.hijayyad@gmail.com>
2023-01-28 13:14:14 +0200

    footer from config file

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -14,14 +14,14 @@ export default function Home(): JSX.Element {
       title={`Hello from ${siteConfig.title}`}
       description="Description will go into a meta tag in <head />"
       wrapperClassName="pb-layout"
-      noFooter
+      // noFooter
     >
       <HomepageHeader />
       <main>
         <HomepageFeatures />
         <GoIntroduction />
       </main>
-      <HomePageFooter />
+      {/* <HomePageFooter /> */}
     </Layout>
   );
 }
