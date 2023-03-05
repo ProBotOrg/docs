19e4f309f3d76e5355f3aa3002882b6f30cd4a7d
ROOKA <r@aae.sa>
2023-03-06 02:18:22 +0300

    update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -10,11 +10,9 @@ export default function Home(): JSX.Element {
 
   return (
     <Layout
-      title={`${siteConfig.title}`}
       description="Get started with ProBot and take your Discord server to new heights with our comprehensive ProBot Docs!
       Discover tips, tricks, and best practices for using this powerful bot."
       wrapperClassName="pb-layout"
-      // noFooter
     >
       <HomepageHeader />
       <main>
