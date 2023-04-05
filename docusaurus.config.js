b495bfb003942ea043ca8443dac2e269506f8195
ROOKA <r@aae.sa>
2023-04-05 14:13:22 +0300

    env for api

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,42 +1,43 @@
-const darkCodeTheme = require("prism-react-renderer/themes/dracula");
+const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: "ProBot Docs",
-  tagline: "ProBot Docs",
-  url: "https://docs.probot.io",
-  baseUrl: "/",
-  onBrokenLinks: "throw",
-  onBrokenMarkdownLinks: "warn",
-  favicon: "img/favicon.ico",
-  organizationName: "ProBotOrg",
-  projectName: "docs",
+  title: 'ProBot Docs',
+  tagline: 'ProBot Docs',
+  url: 'https://docs.probot.io',
+  baseUrl: '/',
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+  favicon: 'img/favicon.ico',
+  organizationName: 'ProBotOrg',
+  projectName: 'docs',
 
   i18n: {
-    defaultLocale: "en",
-    locales: ["en", "ar", "es", "fr", "tr", "vi", "ru"],
+    defaultLocale: 'en',
+    locales: ['en', 'ar', 'es', 'fr', 'tr', 'vi', 'ru'],
     localeConfigs: {
       ar: {
-        direction: "rtl",
+        direction: 'rtl',
       },
     },
   },
 
   presets: [
     [
-      "classic",
+      'classic',
       /** @type {import('@docusaurus/preset-classic').Options} */
       ({
         docs: {
-          sidebarPath: require.resolve("./sidebars.js"),
-          editUrl: "https://github.com/ProBotOrg/docs/blob/master",
+          sidebarPath: require.resolve('./sidebars.js'),
+          editUrl:
+            'https://github.com/ProBotOrg/docs/blob/master',
         },
         theme: {
-          customCss: require.resolve("./src/css/custom.css"),
+          customCss: require.resolve('./src/css/custom.css'),
         },
         blog: false,
         gtag: {
-          trackingID: "G-D4J7KG79HW",
+          trackingID: 'G-D4J7KG79HW',
         },
       }),
     ],
@@ -46,81 +47,81 @@ const config = {
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
       colorMode: {
-        defaultMode: "dark",
+        defaultMode: 'dark',
         disableSwitch: true,
         respectPrefersColorScheme: false,
       },
       algolia: {
-        appId: "BWE1MS63HY",
-        apiKey: "815249683b18febc9f81ffb305465b9f",
-        indexName: "probot",
+        appId: 'BWE1MS63HY',
+        apiKey: process.env.ALGOLIA_API_KEY,
+        indexName: 'probot',
         contextualSearch: true,
         aggregateContent: true,
       },
-      image: "img/probot_docs.png",
+      image: 'img/probot_docs.png',
       navbar: {
-        title: "ProBot Docs",
+        title: 'ProBot Docs',
         logo: {
-          alt: "ProBot Docs",
-          src: "img/logo.png",
+          alt: 'ProBot Docs',
+          src: 'img/logo.png',
         },
         items: [
           {
-            type: "doc",
-            docId: "introduction",
-            position: "left",
-            label: "Introduction",
+            type: 'doc',
+            docId: 'introduction',
+            position: 'left',
+            label: 'Introduction',
           },
-          {to: "https://probot.io/dashboard", label: "Dashboard", position: "left"},
-          {to: "https://discord.com/invite/ProBot", label: "Contact support", position: "left"},
+          {to: 'https://probot.io/dashboard', label: 'Dashboard', position: 'left'},
+          {to: 'https://discord.com/invite/ProBot', label: 'Contact support', position: 'left'},
           {
-            type: "localeDropdown",
-            position: "right",
+            type: 'localeDropdown',
+            position: 'right',
           },
         ],
       },
       footer: {
-        style: "dark",
+        style: 'dark',
         logo: {
-          alt: "ProBot brand logo",
-          src: "img/logo.png",
+          alt: 'ProBot brand logo',
+          src: 'img/logo.png',
           width: 130,
           height: 130,
         },
 
         links: [
           {
-            title: "Docs",
+            title: 'Docs',
             items: [
               {
-                label: "Introduction",
-                to: "/docs/intro",
+                label: 'Introduction',
+                to: '/docs/intro',
               },
             ],
           },
           {
-            title: "Community",
+            title: 'Community',
             items: [
               {
-                label: "Discord",
-                href: "https://discord.gg/probot",
+                label: 'Discord',
+                href: 'https://discord.gg/probot',
               },
               {
-                label: "Reddit",
-                href: "https://reddit.com/r/probot",
+                label: 'Reddit',
+                href: 'https://reddit.com/r/probot',
               },
               {
-                label: "Twitter",
-                href: "https://twitter.com/intent/follow?screen_name=tryProBot",
+                label: 'Twitter',
+                href: 'https://twitter.com/intent/follow?screen_name=tryProBot',
               },
             ],
           },
           {
-            title: "More",
+            title: 'More',
             items: [
               {
-                label: "Commands",
-                href: "https://probot.io/commands",
+                label: 'Commands',
+                href: 'https://probot.io/commands',
               },
             ],
           },
